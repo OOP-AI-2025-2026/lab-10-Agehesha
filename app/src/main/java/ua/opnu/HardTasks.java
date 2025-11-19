a4317c25e64915352a33ff154b80869c1b213293
Agehesha <romashugaev99@gmail.com>
2025-11-19 14:43:48 +0200

    Update HardTasks.java

--- app/src/main/java/ua/opnu/HardTasks.java
+++ app/src/main/java/ua/opnu/HardTasks.java
@@ -1,7 +1,7 @@
 package ua.opnu;
 
-import ua.opnu.util.Product;
 import ua.opnu.util.Order;
+import ua.opnu.util.Product;
 
 import java.util.Comparator;
 import java.util.DoubleSummaryStatistics;
@@ -14,32 +14,70 @@ import java.util.stream.Collectors;
 public class HardTasks {
 
 
+
+    public List<Product> getBooksWithPrice() {
+        return getBooksWithPrice(List.of());
+    }
+
+    public List<Order> getOrdersWithBabyProducts() {
+        return getOrdersWithBabyProducts(List.of());
+    }
+
+    public List<Product> applyDiscountToToys() {
+        return applyDiscountToToys(List.of());
+    }
+
+    public Optional<Product> getCheapestBook() {
+        return getCheapestBook(List.of());
+    }
+
+    public List<Order> getRecentOrders() {
+        return getRecentOrders(List.of());
+    }
+
+    public DoubleSummaryStatistics getBooksStats() {
+        return getBooksStats(List.of());
+    }
+
+    public Map<Integer, Long> getOrdersProductsMap() {
+        return getOrdersProductsMap(List.of());
+    }
+
+    public Map<String, List<Product>> getProductsByCategory() {
+        return getProductsByCategory(List.of());
+    }
+
+
     public List<Product> getBooksWithPrice(List<Product> products) {
         return products.stream()
-                .filter(p -> "Books".equals(p.getCategory()))
+                .filter(p -> "Books".equalsIgnoreCase(p.getCategory()))
                 .filter(p -> p.getPrice() > 100)
                 .collect(Collectors.toList());
     }
 
+
     public List<Order> getOrdersWithBabyProducts(List<Order> orders) {
         return orders.stream()
                 .filter(order -> order.getProducts().stream()
-                        .anyMatch(p -> "Baby".equals(p.getCategory())))
+                        .anyMatch(p -> "Baby".equalsIgnoreCase(p.getCategory())))
                 .collect(Collectors.toList());
     }
 
+
     public List<Product> applyDiscountToToys(List<Product> products) {
         return products.stream()
-                .filter(p -> "Toys".equals(p.getCategory()))
+                .filter(p -> "Toys".equalsIgnoreCase(p.getCategory()))
                 .peek(p -> p.setPrice(p.getPrice() * 0.5))
                 .collect(Collectors.toList());
     }
 
+
     public Optional<Product> getCheapestBook(List<Product> products) {
         return products.stream()
-                .filter(p -> "Books".equals(p.getCategory()))
+                .filter(p -> "Books".equalsIgnoreCase(p.getCategory()))
                 .min(Comparator.comparingDouble(Product::getPrice));
     }
+
 
     public List<Order> getRecentOrders(List<Order> orders) {
         return orders.stream()
@@ -48,12 +86,14 @@ public class HardTasks {
                 .collect(Collectors.toList());
     }
 
+
     public DoubleSummaryStatistics getBooksStats(List<Product> products) {
         return products.stream()
-                .filter(p -> "Books".equals(p.getCategory()))
+                .filter(p -> "Books".equalsIgnoreCase(p.getCategory()))
                 .mapToDouble(Product::getPrice)
                 .summaryStatistics();
     }
+
 
     public Map<Integer, Long> getOrdersProductsMap(List<Order> orders) {
         return orders.stream()
